6d3fd5edab8642a1e6380945e8266dfa828eed7a
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-02-09 16:14:23 +0800

    Add class methods to toggle between tab content
    Define and bind event listeners to select() method
    
    Define TabItem class constructor and method

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -8,27 +8,27 @@ class TabLink {
     this.data = element.dataset.tab
     
     // Using the custom data attribute get the associated Item element
-    this.itemElement = document.querySelector(`.tabs-item[data-tab=${element.dataset.tab}]`)
+    this.itemElement = document.querySelector(`.tabs-item[data-tab="${element.dataset.tab}"]`)
     
     // Using the Item element, create a new instance of the TabItem class
-    this.tabItem = new TabItem(this.tabItem)
+    this.tabItem = new TabItem(this.itemElement)
     
     // Add a click event listener on this instance, calling the select method on click
     element.addEventListener('click', () => this.select())
   };
 
   select() {
-    // Get all of the elements with the tabs-link class
-    const links = document.getElementsByClass('tabs-link')
+    // // Get all of the elements with the tabs-link class
+    const links = document.getElementsByClassName('tabs-link')
 
-    // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
+    // // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
     Array.from(links).forEach(link => link.classList.remove('tabs-link-selected'));
 
-    // Add a class named "tabs-link-selected" to this link
+    // // Add a class named "tabs-link-selected" to this link
     this.element.classList.toggle('tabs-link-selected')
     
-    // Call the select method on the item associated with this link
-    this.element.select()
+    // // Call the select method on the item associated with this link
+    this.tabItem.select()
   }
 }
 
@@ -36,16 +36,18 @@ class TabItem {
   constructor(element) {
     // Assign this.element to the passed in element
      this.element = element
+
   }
 
   select() {
     // Select all ".tabs-item" elements from the DOM
-    const items = document.querySlectAll('.tabs-item')
-
+    const items = document.querySelectorAll('.tabs-item')
+ 
     // Remove the class "tabs-item-selected" from each element
     items.forEach(item => item.classList.remove('tabs-item-selected'))
     // Add a class named "tabs-item-selected" to this element
     this.element.classList.add('tabs-item-selected')
+
   }
 }
 
